dbc01cb0e804683e814daab18bc49ac90fe33c35
Cao Tri Ngoc <tringoc2077@gmail.com>
2025-08-09 08:37:13 +0700

    [notification service]: set up firebase SDK, base project, entity notification

--- notification-service/src/app.module.ts
+++ notification-service/src/app.module.ts
@@ -1,9 +1,35 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MailerModule } from './mailer/mailer.module';
+import { FcmModule } from './fcm/fcm.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Notification } from './entities/notification.entity';
 
 @Module({
-  imports: [],
+  imports: [
+    MailerModule,
+    FcmModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        type: 'postgres',
+        host: config.get<string>('DATABASE_HOST'),
+        port: 5432,
+        username: config.get<string>('DATABASE_USERNAME'),
+        password: config.get<string>('DATABASE_PASSWORD'),
+        database: config.get<string>('DATABASE_NAME'),
+        entities: [Notification],
+        synchronize: true,
+        ssl: true,
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ notification-service/src/entities/notification.entity.ts
@@ -0,0 +1,62 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+export enum Channels {
+  EMAIL = 'EMAIL',
+  PUSH = 'PUSH',
+  IN_APP = 'IN_APP',
+}
+
+export enum Status {
+  SENT = 'SENT',
+  FAILED = 'FAILED',
+  PENDING = 'PENDING',
+}
+
+export enum StatusInApp {
+  READ = 'READ',
+  UNREAD = 'UNREAD',
+}
+
+@Entity('notifications')
+export class Notification {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  userId: string;
+
+  @Column()
+  title: string;
+
+  @Column('text')
+  message: string;
+
+  @Column({ nullable: true })
+  type?: string; // RECOMMENDATION, BILLING, SYSTEM...
+
+  @Column({ nullable: true })
+  channels?: Channels;
+
+  @Column({ nullable: true })
+  status?: Status | StatusInApp;
+
+  @Column({ nullable: true })
+  metadata?: {
+    movieId?: string;
+    link?: string;
+  };
+
+  @Column({ default: false })
+  isRead: boolean;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  readAt: Date;
+}
