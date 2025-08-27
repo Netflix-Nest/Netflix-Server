e84f97531506147a330e6235115252a4b9152412
Cao Tri Ngoc <tringoc2077@gmail.com>
2025-08-27 16:37:09 +0700

    [gateway]: config cors, api versioning

--- api-gateway/src/main.ts
+++ api-gateway/src/main.ts
@@ -1,7 +1,7 @@
 import { NestFactory, Reflector } from "@nestjs/core";
 import {
-	LoggingInterceptor,
-	TransformInterceptor,
+  LoggingInterceptor,
+  TransformInterceptor,
 } from "@netflix-clone/common";
 import { AppModule } from "./app.module";
 import { AllExceptionsFilter } from "@netflix-clone/common";
@@ -13,39 +13,39 @@ import { ThrottlerGuard } from "@nestjs/throttler";
 import { JwtAuthGuard } from "./common/guards/jwt-auth.guard";
 
 async function bootstrap() {
-	const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule);
 
-	app.useGlobalFilters(new AllExceptionsFilter());
-	const configService = app.get(ConfigService);
-	const reflector = app.get(Reflector);
+  app.useGlobalFilters(new AllExceptionsFilter());
+  const configService = app.get(ConfigService);
+  const reflector = app.get(Reflector);
 
-	app.useGlobalGuards(new JwtAuthGuard(reflector));
+  app.useGlobalGuards(new JwtAuthGuard(reflector));
 
-	app.useGlobalInterceptors(
-		new LoggingInterceptor(),
-		new TransformInterceptor()
-	);
+  app.useGlobalInterceptors(
+    new LoggingInterceptor(),
+    new TransformInterceptor()
+  );
 
-	app.enableCors({
-		origin: configService.get<string>("ORIGIN"),
-		methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
-		credentials: true,
-	});
+  app.enableCors({
+    origin: configService.get<string>("ALLOW_ORIGIN"),
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    credentials: true,
+  });
 
-	app.useGlobalPipes(
-		new ValidationPipe({
-			whitelist: true,
-		})
-	);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+    })
+  );
 
-	app.use(cookieParser());
-	//config versioning
-	// app.setGlobalPrefix("api"); //api, /v nestjs auto add
-	// app.enableVersioning({
-	//   type: VersioningType.URI,
-	//   defaultVersion: ["1", "2"],
-	// });
+  app.use(cookieParser());
+  // config versioning
+  app.setGlobalPrefix("api"); //api, /v nestjs auto add
+  app.enableVersioning({
+    type: VersioningType.URI,
+    defaultVersion: ["1"],
+  });
 
-	await app.listen(configService.get<number>("PORT") || 3000);
+  await app.listen(configService.get<number>("PORT") || 3000);
 }
 bootstrap();
